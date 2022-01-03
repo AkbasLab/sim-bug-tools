152479dd7b84536906c021f195c5c8e1ca53337f
Quentin Goss <gossq@my.erau.edu>
2022-01-02 20:14:51 -0500

    completed bug_builder tool.

---
+++ install.bat
@@ -0,0 +1,1 @@
+python -m pip install --upgrade --no-deps --force-reinstall dist\sim_bug_tools_gossq-0.0.1-py3-none-any.whl
--- src/sim_bug_tools/rng/lds/__init__.py
+++ src/sim_bug_tools/rng/lds/__init__.py
@@ -1,7 +1,1 @@
-"""
-The LDS package provides tools for generating low discrepancy sequences and measuring coverage of
-those sequences.
-"""
 
-import avtools.rng.lds.sequences
-import avtools.rng.lds.coverage
