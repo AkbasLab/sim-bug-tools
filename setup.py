b7af3cbb16ef8ab7fba9bc3c10489d36c79cb5e4
Quentin Goss <gossq@my.erau.edu>
2022-01-02 19:13:08 -0500

    First package.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     package_dir = {"": "src"},
     packages = setuptools.find_packages(where="src"),
     python_requires = ">= 3.9",
-    requires = [
+    install_requires = [
         "numpy >= 1.21.1", 
         "matplotlib >= 3.4.2", 
         "scipy >= 1.7.1",
