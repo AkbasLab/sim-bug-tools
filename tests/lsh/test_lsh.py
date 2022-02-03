0e5f065d028530989e0f73e65b9e6f687d20c7f3
Quentin Goss <gossq@my.erau.edu>
2022-02-03 15:50:34 -0500

    Locality Sensitive Hashing implemented.

--- tests/lsh/test_lsh.py
+++ tests/lsh/test_lsh.py
@@ -1,12 +1,20 @@
 import unittest
 import sim_bug_tools.lshash as lshash
+import numpy as np
+import sim_bug_tools.structs as structs
 
 class TestLSH(unittest.TestCase):
 
     def test_lsh(self):
-        print("\n\n")
+        # print("\n\n")
 
-        lsh = lshash.LSHash(6,2)
+        n_dim = 2
+        lsh = lshash.LSHash(6,n_dim,n_hash_generators=10)
 
-        print("\n\n")
+        rng = np.random.default_rng(3)
+        points = [structs.Point(np.round(p,1)) for p in rng.uniform(size=(1000, n_dim))]
+        [lsh.index(p) for p in points]
+        
+
+        # print("\n\n")
         return
