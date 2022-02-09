b7199b9c4f253991e1aa0f18e7a8318879102d13
Quentin Goss <gossq@my.erau.edu>
2022-02-09 16:35:27 -0500

    [p] TraCI client.

--- src/sim_bug_tools/simulators.py
+++ src/sim_bug_tools/simulators.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 import enum
+import warnings
 import sim_bug_tools.structs as structs
 import sim_bug_tools.rng.lds.sequences as sequences
 from sim_bug_tools.rng.rrt import RapidlyExploringRandomTree
@@ -8,9 +9,7 @@ import pandas as pd
 import numpy as np
 import json
 import os
-
-def from_dict(d : dict):
-    return
+import traci
 
 class State(enum.Enum):
     PAUSED = "PAUSED"
@@ -872,7 +871,84 @@ class SimpleSimulatorKnownBugsRRT(SimpleSimulatorKnownBugs):
     
 
 
-    
-    
+class TraCIClient(Simulator):
+    def __init__(self, 
+        config : dict, 
+        priority : int = 1,  
+        **kwargs):
+        """
+        Barebones TraCI client.
 
-    
+        --- Parameters ---
+        priority : int
+            Priority of clients. MUST BE UNIQUE
+        config : dict
+            SUMO arguments stored as a python dictionary.
+        """
+        self._priority = priority
+        self._config = config
+        self.connect()
+        return
+
+    @property
+    def priority(self) -> int:
+        """
+        Priority of TraCI client.
+        """
+        return self._priority
+
+    @property
+    def config(self) -> dict:
+        """
+        SUMO arguments stored as a python dictionary.
+        """
+        return self._config
+
+    def run_to_end(self):
+        """
+        Runs the client until the end.
+        """
+        while traci.simulation.getMinExpectedNumber() > 0:
+            traci.simulationStep()
+            # more traci commands
+        return
+
+    def close(self):
+        """
+        Closes the client.
+        """
+        traci.close()
+        return
+
+
+    def connect(self):
+        """
+        Start or initialize the TraCI connection.
+        """
+        warnings.simplefilter("ignore", ResourceWarning)
+        # Start the traci server with the first client
+        if self.priority == 1:
+            cmd = []
+
+            for key, val in self.config.items():
+                if key == "gui":
+                    sumo = "sumo"
+                    if val: sumo +="-gui"
+                    cmd.append(sumo)
+                    continue
+                
+                if key == "--remote-port":
+                    continue
+
+                cmd.append(key)
+                cmd.append(str(val))
+                continue
+
+            traci.start(cmd,port=self.config["--remote-port"])
+            traci.setOrder(self.priority)
+            return
+        
+        # Initialize every client after the first.
+        traci.init(port=self.config["--remote-port"])
+        traci.setOrder(self.priority)
+        return    
