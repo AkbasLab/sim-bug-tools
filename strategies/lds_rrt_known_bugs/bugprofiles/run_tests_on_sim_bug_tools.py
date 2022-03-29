d2ac02818a6e691c0f29156c23109e3b98c559c8
JakobH3 <60677704+JakobH3@users.noreply.github.com>
2022-03-29 15:44:02 -0400

    Updated to unittest framework
    Updated run_tests_on_sim_bug_tools to run on the unittest framework and added ignore to the /out and /test_bug_profiles folders

--- strategies/lds_rrt_known_bugs/bugprofiles/run_tests_on_sim_bug_tools.py
+++ strategies/lds_rrt_known_bugs/bugprofiles/run_tests_on_sim_bug_tools.py
@@ -1,5 +1,6 @@
 # Add the parent directory to the path
 import os, sys
+import unittest
 UNITTEST_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(UNITTEST_DIR))
 import sim_bug_tools.structs as structs
@@ -16,280 +17,291 @@ import numpy as np
 import json
 
 
-def test_simulator_known_bugs(i : int):
-    with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
-        for line in f:
-            hhh = json.loads(line)
-            break
-    bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    seq = sequences.RandomSequence(
-        domain, 
-        ["dim_%d" % n for n in range(n_dim)],
-        seed = 300
-    )
+class RunTestsOnSimulators(unittest.TestCase):
 
-    sim = simulator.SimpleSimulatorKnownBugs(
-        bug_profile, seq, 
-        file_name = UNITTEST_DIR + "/out/sskbRandomSequence" + str(i) + ".tsv"
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.long_walk_on_enter()
-    sim.run(10)
-
-    
-    return
-
-def test_simulator_known_bugs_faure_sequence(i : int):
-    with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
-        for line in f:
-            hhh = json.loads(line)
-            break
-    bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    seq = sequences.FaureSequence(
-        domain, 
-        ["dim_%d" % n for n in range(n_dim)],
-        seed = 300
-    )
-
-    sim = simulator.SimpleSimulatorKnownBugs(
-        bug_profile, seq, 
-        file_name = UNITTEST_DIR + "/out/sskbFaureSequence" + str(i) + ".tsv"
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.long_walk_on_enter()
-    sim.run(10)
-
+    def simulator_known_bugs(self, i : int):
+        with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
+            for line in f:
+                hhh = json.loads(line)
+                break
+        bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
         
-    return
-
-def test_simulator_known_bugs_halton_sequence(i : int):
-    with open(UNITTEST_DIR +"/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
-        for line in f:
-            hhh = json.loads(line)
-            break
-    bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    seq = sequences.HaltonSequence(
-        domain, 
-        ["dim_%d" % n for n in range(n_dim)],
-        seed = 300
-    )
-
-    sim = simulator.SimpleSimulatorKnownBugs(
-        bug_profile, seq, 
-        file_name = UNITTEST_DIR + "/out/sskbHaltonSequence" + str(i) + ".tsv"
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.long_walk_on_enter()
-    sim.run(10)
-
-    
-    
-    return
-
-def test_simulator_known_bugs_sobol_sequence(i : int):
-    with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
-        for line in f:
-            hhh = json.loads(line)
-            break
-    bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    seq = sequences.SobolSequence(
-        domain, 
-        ["dim_%d" % n for n in range(n_dim)],
-        seed = 300
-    )
-
-    sim = simulator.SimpleSimulatorKnownBugs(
-        bug_profile, seq, 
-        file_name = UNITTEST_DIR + "/out/sskbSobolSequence" + str(i) + ".tsv"
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.long_walk_on_enter()
-    sim.run(10)
-
-        
-    return
-
-
-def test_simulator_known_bugs_lattice_sequence(i : int):
-    with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
-        for line in f:
-            hhh = json.loads(line)
-            break
-    bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    seq = sequences.LatticeSequence(
-        domain, 
-        ["dim_%d" % n for n in range(n_dim)],
-        seed = 300
-    )
-
-    sim = simulator.SimpleSimulatorKnownBugs(
-        bug_profile, seq, 
-        file_name = UNITTEST_DIR + "/out/sskbLatticeSequence" + str(i) + ".tsv"
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.long_walk_on_enter()
-    sim.run(10)
-
-       
-    return
-
-
-def test_simulator_known_bugs_rrt(i : int):
-    bug_profile = [structs.Domain([(0,1) for n in range(4)])]
-    
-    n_dim = len(bug_profile[0])
-    domain = structs.Domain([(0,1) for n in range(n_dim)])
-    axes_names = ["dim_%d" % n for n in range(n_dim)]
-    seq = sequences.RandomSequence(
-        domain, axes_names, seed = 300
-    )
-    rrt = RapidlyExploringRandomTree(
-        sequences.RandomSequence(
-            domain, axes_names, seed = 555
-        ),
-        step_size = 0.01,
-        exploration_radius = 1
-    )
-    
-    sim = simulator.SimpleSimulatorKnownBugsRRT(
-        bug_profile = bug_profile,
-        sequence = seq,
-        rrt = rrt,
-        n_branches = 5,
-        file_name = "%s/out/sskbrrt.tsv" % UNITTEST_DIR,
-        log_to_console = False
-    )
-
-    if os.path.exists(sim.file_name):
-        os.remove(sim.file_name)
-
-    sim.run(10)
-    sim.local_search_on_enter()
-    sim.run(14)
-
-    
-
-    df = pd.read_csv(sim.file_name, sep="\t")
-    
-    return
-
-
-def random_size_domain(n_dim : int, a : float, b : float, random : Random):
-    return structs.Domain( 
-        np.round( [(0, random.uniform(a, b,)) for _ in range(n_dim)], decimals=2)  
-    ) 
-
-
-
-def generate_bug_profile(n_dim : int, n_clusters : int, i : int):
-    
-    file_name = UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json"
-
-    if os.path.exists(file_name):
-                os.remove(file_name)
-
-
-    cluster_size_min = 0.1
-    cluster_size_max = 0.2
-    n_clusters = 1
-    seed = 0
-    n_profiles = 30
-    random = Random(seed)
-
-    normal_domain = structs.Domain([(0,1) for _ in range(n_dim)])
-    random = Random(seed) 
-
-    # We need to use the random sequence or it will conflict with our results.
-    seq = sequences.RandomSequence(
-                normal_domain, 
-                ["dim_%d" % n for n in range(n_dim)],
-                seed = 300
-            )
-
-    # Construct the bug builder object
-    bug_builder = bugger.BugBuilder(
-            location_domain = normal_domain,
-            size_domain = random_size_domain(
-                n_dim, cluster_size_min, cluster_size_max, random),
-            sequence = seq,
-            random = random
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        seq = sequences.RandomSequence(
+            domain, 
+            ["dim_%d" % n for n in range(n_dim)],
+            seed = 300
         )
 
-    # Generate the profiles
-    profiles = []
-    for i_profile in range(n_profiles):
-        clusters : list[structs.Domain] = []
-        for i_bug in range(n_clusters):
-            # Generate a cluster
-            clusters.append( bug_builder.build_bug().as_json() )
+        sim = simulator.SimpleSimulatorKnownBugs(
+            bug_profile, seq, 
+            file_name = UNITTEST_DIR + "/out/sskbRandomSequence" + str(i) + ".tsv"
+        )
 
-            # Scramble the size of the next cluster.
-            bug_builder.size_domain = random_size_domain(
-                n_dim, cluster_size_min, cluster_size_max, random)
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.long_walk_on_enter()
+        sim.run(10)
+
+        
+        return
+
+    def simulator_known_bugs_faure_sequence(self, i : int):
+        with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
+            for line in f:
+                hhh = json.loads(line)
+                break
+        bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
+        
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        seq = sequences.FaureSequence(
+            domain, 
+            ["dim_%d" % n for n in range(n_dim)],
+            seed = 300
+        )
+
+        sim = simulator.SimpleSimulatorKnownBugs(
+            bug_profile, seq, 
+            file_name = UNITTEST_DIR + "/out/sskbFaureSequence" + str(i) + ".tsv"
+        )
+
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.long_walk_on_enter()
+        sim.run(10)
+
+            
+        return
+
+    def simulator_known_bugs_halton_sequence(self, i : int):
+        with open(UNITTEST_DIR +"/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
+            for line in f:
+                hhh = json.loads(line)
+                break
+        bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
+        
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        seq = sequences.HaltonSequence(
+            domain, 
+            ["dim_%d" % n for n in range(n_dim)],
+            seed = 300
+        )
+
+        sim = simulator.SimpleSimulatorKnownBugs(
+            bug_profile, seq, 
+            file_name = UNITTEST_DIR + "/out/sskbHaltonSequence" + str(i) + ".tsv"
+        )
+
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.long_walk_on_enter()
+        sim.run(10)
+
+        
+        
+        return
+
+    def simulator_known_bugs_sobol_sequence(self, i : int):
+        with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
+            for line in f:
+                hhh = json.loads(line)
+                break
+        bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
+        
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        seq = sequences.SobolSequence(
+            domain, 
+            ["dim_%d" % n for n in range(n_dim)],
+            seed = 300
+        )
+
+        sim = simulator.SimpleSimulatorKnownBugs(
+            bug_profile, seq, 
+            file_name = UNITTEST_DIR + "/out/sskbSobolSequence" + str(i) + ".tsv"
+        )
+
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.long_walk_on_enter()
+        sim.run(10)
+
+            
+        return
+
+
+    def simulator_known_bugs_lattice_sequence(self, i : int):
+        with open(UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json", "r") as f:
+            for line in f:
+                hhh = json.loads(line)
+                break
+        bug_profile = [structs.Domain.from_json(d) for d in hhh[0]]
+        
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        seq = sequences.LatticeSequence(
+            domain, 
+            ["dim_%d" % n for n in range(n_dim)],
+            seed = 300
+        )
+
+        sim = simulator.SimpleSimulatorKnownBugs(
+            bug_profile, seq, 
+            file_name = UNITTEST_DIR + "/out/sskbLatticeSequence" + str(i) + ".tsv"
+        )
+
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.long_walk_on_enter()
+        sim.run(10)
+
+        
+        return
+
+
+    def simulator_known_bugs_rrt(self, i : int):
+        bug_profile = [structs.Domain([(0,1) for n in range(4)])]
+        
+        n_dim = len(bug_profile[0])
+        domain = structs.Domain([(0,1) for n in range(n_dim)])
+        axes_names = ["dim_%d" % n for n in range(n_dim)]
+        seq = sequences.RandomSequence(
+            domain, axes_names, seed = 300
+        )
+        rrt = RapidlyExploringRandomTree(
+            sequences.RandomSequence(
+                domain, axes_names, seed = 555
+            ),
+            step_size = 0.01,
+            exploration_radius = 1
+        )
+        
+        sim = simulator.SimpleSimulatorKnownBugsRRT(
+            bug_profile = bug_profile,
+            sequence = seq,
+            rrt = rrt,
+            n_branches = 5,
+            file_name = "%s/out/sskbrrt.tsv" % UNITTEST_DIR,
+            log_to_console = False
+        )
+
+        if os.path.exists(sim.file_name):
+            os.remove(sim.file_name)
+
+        sim.run(10)
+        sim.local_search_on_enter()
+        sim.run(14)
+
+        
+
+        df = pd.read_csv(sim.file_name, sep="\t")
+        
+        return
+
+
+    def random_size_domain(self, n_dim : int, a : float, b : float, random : Random):
+        return structs.Domain( 
+            np.round( [(0, random.uniform(a, b,)) for _ in range(n_dim)], decimals=2)  
+        ) 
+
+
+
+    def generate_bug_profile(self, n_dim : int, n_clusters : int, i : int):
+        
+        file_name = UNITTEST_DIR + "/test_bug_profiles/test_bugs" + str(i) + ".json"
+
+        if os.path.exists(file_name):
+                    os.remove(file_name)
+
+
+        cluster_size_min = 0.1
+        cluster_size_max = 0.2
+        n_clusters = 1
+        seed = 0
+        n_profiles = 30
+        random = Random(seed)
+
+        normal_domain = structs.Domain([(0,1) for _ in range(n_dim)])
+        random = Random(seed) 
+
+        # We need to use the random sequence or it will conflict with our results.
+        seq = sequences.RandomSequence(
+                    normal_domain, 
+                    ["dim_%d" % n for n in range(n_dim)],
+                    seed = 300
+                )
+
+        # Construct the bug builder object
+        bug_builder = bugger.BugBuilder(
+                location_domain = normal_domain,
+                size_domain = self.random_size_domain(
+                    n_dim, cluster_size_min, cluster_size_max, random),
+                sequence = seq,
+                random = random
+            )
+
+        # Generate the profiles
+        profiles = []
+        for i_profile in range(n_profiles):
+            clusters : list[structs.Domain] = []
+            for i_bug in range(n_clusters):
+                # Generate a cluster
+                clusters.append( bug_builder.build_bug().as_json() )
+
+                # Scramble the size of the next cluster.
+                bug_builder.size_domain = self.random_size_domain(
+                    n_dim, cluster_size_min, cluster_size_max, random)
+                continue
+            profiles.append(clusters)
+            
             continue
-        profiles.append(clusters)
-        
-        continue
 
-    #  Dump to file
-    with open(file_name, "w") as f:
-        f.write(json.dumps(profiles)) 
+        #  Dump to file
+        with open(file_name, "w") as f:
+            f.write(json.dumps(profiles)) 
 
-    print("Profiles saved to \"%s\"." % file_name)
+        print("Profiles saved to \"%s\"." % file_name)
+            
+            
         
-        
-    
-    return
+        return
+
+    def test_all_cluster_dimension_combiniation(self):
+        i = 0
+        dimension_range = [2,5,10,50]
+        cluster_range = range(11)
+
+        for dimension in dimension_range:
+            for cluster in cluster_range:
+                self.generate_bug_profile(dimension,cluster,i)
+                self.simulator_known_bugs(i)
+                self.simulator_known_bugs_faure_sequence(i)
+                self.simulator_known_bugs_lattice_sequence(i)
+                self.simulator_known_bugs_halton_sequence(i)
+                self.simulator_known_bugs_rrt(i)
+                self.simulator_known_bugs_sobol_sequence(i)
+                i = i + 1
+        print("tests complete")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
 
 #Test number
-i = 0
-dimension_range = [2,5,10,50]
-cluster_range = range(10)
 
-for dimension in dimension_range:
-    for cluster in cluster_range:
-        generate_bug_profile(dimension,cluster,i)
-        test_simulator_known_bugs(i)
-        test_simulator_known_bugs_faure_sequence(i)
-        test_simulator_known_bugs_lattice_sequence(i)
-        test_simulator_known_bugs_halton_sequence(i)
-        test_simulator_known_bugs_rrt(i)
-        test_simulator_known_bugs_sobol_sequence(i)
-        i = i + 1
 
 
