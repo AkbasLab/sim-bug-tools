7c03f050643ab2b7a7123ce67ce7d8d8da84612e
Quentin Goss <gossq@my.erau.edu>
2022-06-09 18:09:59 -0400

    Simple Example complete.

--- strategies/simple_example/simple.py
+++ strategies/simple_example/simple.py
@@ -55,49 +55,76 @@ class Simple(simulator.Simulator):
         return self._client
 
     def _run_sumo_scenario(self, 
-        dist_from_stop : float, speed_start : float):
+        dist_from_stop : float, speed_start : float) -> bool:
+        """
+        dist_from_stop : float
+            Distance from stop line (m)
+        speed_start : float
+            Initial speed of AV (kph)
+        """
 
         # Start the client
         self._client = TraCIClient(self.config)
 
         # Add the route
-        route_id = "route_1"
-        traci.route.add(route_id, ["E0","E1"])
+        route_id = "R0"
+        traci.route.add(route_id, ["Ewarmup", "E0", "E1"])
 
         # Add vehicle
         av_id = "av"
         traci.vehicle.add(
             av_id, 
-            route_id, 
-            departPos = 1000 - dist_from_stop,
+            route_id
         )
+
+        # Convert speed from kps -> mps
+        speed_start = speed_start/3.6
+
+        # Get the vehicle up to speed.
+        traci.vehicle.setSpeedMode(av_id, 0)
 
         traci.vehicle.setMaxSpeed(av_id, 100)
         traci.vehicle.setSpeed(av_id, speed_start)
+
+        default_accel = traci.vehicle.getAccel(av_id)
         traci.vehicle.setAccel(av_id, speed_start)
         
-
-        comfortable_deccel = 4.5
-        is_comfy = True
+        
 
         # Simulation loop
-        i = 0
+        is_comfortable = True
+        is_warmup = True
         while traci.simulation.getMinExpectedNumber() > 0:
-
-            accel = traci.vehicle.getAcceleration(av_id)
+            if is_warmup:
+                # Get the AV up to speed
+                speed = traci.vehicle.getSpeed(av_id)
+                # print(speed)
+                if speed == speed_start:
+                    # Transport to the new edge
+                    traci.vehicle.moveTo(av_id, "E0_0", 0)
+                    traci.vehicle.setSpeedMode(av_id, 100 - dist_from_stop)
+                    traci.vehicle.setAccel(av_id, default_accel)
+                    is_warmup = False
+                traci.simulationStep()
+                continue
+            
+            # Vehicle is up to speed
+            e_stop = traci.simulation.getEmergencyStoppingVehiclesNumber()
+            if e_stop:
+                is_comfortable = False
+                break
+            
+            # AV stopped At traffic light
             speed = traci.vehicle.getSpeed(av_id)
-            print("%.2fkph %.2fm/s^2" % (speed, accel))
-
-            i += 1
-            if i > 20:
+            if not int(speed):
                 break
             
             traci.simulationStep()
             continue
 
+        
+
         # Sim complete
         self.client.close()
 
-        print()
-        print("is_comfortable: %s" % is_comfy)
-        return
+        return is_comfortable
--- strategies/simple_example/unit_tests/test_simple.py
+++ strategies/simple_example/unit_tests/test_simple.py
@@ -3,14 +3,38 @@ FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(FILE_DIR))
 from simple import Simple
 
+import pandas as pd
 import unittest
 
 class TestSimple(unittest.TestCase):
     def test_simple(self):
         print("\n\n")
-        sim = Simple(file_name = "%s/out/simple.tsv" % FILE_DIR)
+        sim = Simple(file_name = "%s/out/simple.csv" % FILE_DIR)
+        
+        dist_from_stop = []
+        speed_start = []
+        is_comfortable = []
 
-        sim._run_sumo_scenario(1000, 100)
+        
+        i = 0; total = 100*51
+        for dfs in range(1,100+1):
+            for ss in range(0,50+1):
+                i += 1
+                print("RUN %d of %d" % (i,total))
+                dist_from_stop.append(dfs)
+                speed_start.append(ss)
+                is_comfortable.append(
+                    sim._run_sumo_scenario(dfs, ss)
+                )
+                break
+            break
+
+        df = pd.DataFrame({
+            "dist_from_stop" : dist_from_stop,
+            "speed_start" : speed_start,
+            "is_comfortable" : is_comfortable
+        })
+        df.to_csv("%s/out/simple.csv" % FILE_DIR, index=False)
         print("\n\n")
         return
 
