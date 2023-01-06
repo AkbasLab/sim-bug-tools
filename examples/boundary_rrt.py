cff00f38bb055e20876e17ae07a379c721060d44
thomj130 <thomj130@my.erau.edu>
2023-01-06 17:43:45 -0500

    Modified examples to account for new updates.
    - Addition of Scaler objects.

--- examples/boundary_rrt.py
+++ examples/boundary_rrt.py
@@ -1,4 +1,16 @@
 import numpy as np
+from numpy import ndarray, float64
+from sim_bug_tools.exploration.boundary_core.adherer import (
+    BoundaryLostException,
+    SampleOutOfBoundsException,
+)
+import json
+
+DATA_OUTPUT = "output.json"
+
+ADHERER_VERSION = "v1"
+ENVELOPE_TYPE = "sphere"
+ndims = 30
 
 """
 We can use the pre-defined boundary exploration strategy "BoundaryRRT" to 
@@ -21,12 +33,18 @@ p, is a target value, otherwise it is a non-target value.
 # Importing commonly used structures
 from sim_bug_tools.structs import Domain, Point, Spheroid
 
-# To illustrate this visually, we will stick to only 3 dimensions
-ndims = 3
+# To illustrate this visually, use only 3 dimensions
 
 # We need to define some basic parameters for later:
 d = 0.05  # the distance we jump with each sample
-theta = np.pi / 180 * 5  # 5 degree rotations for finding the boundary
+
+# Constant Adherence Strategy
+delta_theta = 15 * np.pi / 180  # 5 degree rotations for finding the boundary
+
+# Exponential Adherence Strategy
+theta0 = np.pi / 180 * 90  # Exponential Adherence Strategy: 90 degree start rotation
+r = 2
+N = 4
 
 # We will be using a simple sphere as our envelope, for simplicity's sake.
 class Sphere:
@@ -37,21 +55,51 @@ class Sphere:
     def __contains__(self, p: Point):
         return self.loc.distance_to(p) <= self.radius
 
+    def theoretical_normal(self, p: Point) -> ndarray:
+        dir = (p - self.loc).array
+        return dir / np.linalg.norm(dir)
 
-envelope = Sphere(Point([0.5 for x in range(ndims-1)] + [0]), 0.25)
 
+class Cube:
+    def __init__(self, center_loc: Point, length: float):
+        self.loc = center_loc
+        self.length = length
+
+        half_length = length / 2
+        _high = center_loc + Point([half_length for i in range(len(center_loc))])
+        _low = center_loc - Point([half_length for i in range(len(center_loc))])
+        self._domain = Domain.from_bounding_points(_low, _high)
+
+    def __contains__(self, p: Point):
+        return p in self._domain
+
+
+# Setting up envelope:
+center_loc = Point([0.5 for x in range(ndims)])
+
+if ENVELOPE_TYPE == "sphere":
+    envelope = Sphere(center_loc, 0.25)
+elif ENVELOPE_TYPE == "cube":
+    envelope = Cube(center_loc, 0.25)
+
+# Setting up classifying function:
 # If the point lies WITHIN the envelope, it is classified as a target sample
 classifier = lambda p: p in envelope
+
+
 domain = Domain.normalized(ndims)
 
 # We are using the point below as our initial sample...
 # Generate an initial target sample:
-is_target = False 
+is_target = False
 while not is_target:
     v = np.random.randn(ndims)
     v = v / np.linalg.norm(v)
-    t0 = Point(np.array(envelope.loc) + v * (envelope.radius * 0.90))
-    is_target = t0 in domain 
+    if ENVELOPE_TYPE == "sphere":
+        t0 = Point(np.array(envelope.loc) + v * (envelope.radius * 0.90))
+    elif ENVELOPE_TYPE == "cube":
+        t0 = Point(np.array(envelope.loc) + v * (envelope.length / 2 * 0.90))
+    is_target = t0 in envelope
     if not is_target:
         print("Retrying...")
 
@@ -59,43 +107,124 @@ while not is_target:
 # Since we don't know if the target sample is on the boundary, we must surface:
 from sim_bug_tools.exploration.boundary_core.surfacer import find_surface
 
-node0, _ = find_surface(classifier, t0, d)
+# By default, find_surface randomly picks a direction; however, a better
+# solution is to find both a target and non-target sample, using their
+# normalized displacement vector for the direction of travel (v).
+tmp = find_surface(classifier, t0, d, domain)
+if tmp is not None:
+    node0, _, _ = tmp
+else:
+    print("none?")
 b0, n0 = node0  # initial boundary point and surface vector
 
 # Now that we have our inital boundary point and its orthonormal surface vector,
 # we now construct our surface explorer...
 
 # First, we must select and build our adherence strategy's factory, we will use
-# the predefined solution:
-from sim_bug_tools.exploration.brrt_std.adherer import BoundaryAdherenceFactory
+# the predefined solutions. BAF1 is Constant Adherer, and BAF2 is Exponential.
+from sim_bug_tools.exploration.brrt_std.adherer import (
+    ConstantAdherenceFactory as ConstAdherer,
+)
+from sim_bug_tools.exploration.brrt_v2.adherer import (
+    ExponentialAdherenceFactory as ExpAdherer,
+)
 
 sphere_scaler = Spheroid(d)
-adhFactory = BoundaryAdherenceFactory(classifier, domain, sphere_scaler, theta)
 
-# Now, we can create our explorer:
+if ADHERER_VERSION == "v1":
+    adhFactory = ConstAdherer(classifier, domain, sphere_scaler, delta_theta, True)
+elif ADHERER_VERSION == "v2":
+    adhFactory = ExpAdherer(classifier, domain, d, theta0, r, N, True)
+
+# Now, we can create our explorer, in our case we will use our default Boundary
+# Rapidly Exploring Random Tree:
 from sim_bug_tools.exploration.brrt_std.brrt import BoundaryRRT
 
-brrt = BoundaryRRT(b0, n0, adhFactory)
-
-# Finally, we can start using this explorer to sample from the boundary:
-nsamples = 100
-nbatches = 20
-
 if ndims == 3:
-
     import matplotlib.pyplot as plt
-
     from sim_bug_tools.graphics import Grapher  # (used for plotting)
 
     grapher = Grapher(is3d=True, domain=Domain.normalized(ndims))
+    if ENVELOPE_TYPE == "sphere":
+        grapher.draw_sphere(envelope.loc, envelope.radius)
+    # a = grapher.add_arrow(b0, n0)
+    # plt.pause(0.01)
 
+# Finally, we can start using this explorer to sample from the boundary:
+nsamples = 500
 
-for i in range(nbatches):
-    nodes = brrt.expand_by(nsamples)
-    points = list(map(lambda s: s[0], nodes))
+i = 0
+bp_k = 1  # Back-prop depth, 0 for no backprop
+batch = 1
 
-    if ndims == 3:
-        grapher.plot_all_points(points)
-        plt.pause(0.05)
-    
-    input("Press enter to continue...")
+backprop_enabled = bp_k > 0
+
+brrt = BoundaryRRT(b0, n0, adhFactory)
+
+i = 0
+all_points: list[tuple[Point, bool]] = []
+all_exp_steps = []
+exp_step = {"b-node": None, "all-points": []}
+errs = 0
+out_of_bounds_count = 0
+
+while brrt.step_count < (nsamples):
+    try:
+        tmp = brrt.step()
+        all_points.append(tmp)
+        exp_step["all-points"].append((tuple(tmp[0]), tmp[1]))
+
+    except BoundaryLostException:
+        # Occurs when the adherer fails to find the boundary
+        errs += 1
+
+    except SampleOutOfBoundsException as e:
+        # Occurs (iff fail_out_of_bounds enabled) when the sample is outside the
+        # set domain (e.g. outside the normalized domain.) If not enabled, then
+        # it will simply adhere to the boundary between the domain edge and the
+        # envelope.
+        out_of_bounds_count += 1
+
+    if brrt.step_count > i:
+        if backprop_enabled:
+            brrt.back_propegate_prev(bp_k)
+
+        exp_step["b-node"] = [tuple(x) for x in brrt.boundary[-1]]
+        all_exp_steps.append(exp_step)
+        exp_step = {"b-node": None, "all-points": []}
+        i = brrt.step_count
+
+# How to determine the non-boundary points
+non_boundary_points = [
+    (tuple(p), bool(cls))
+    for p, cls in all_points
+    if p not in map(lambda node: node[0], brrt.boundary)
+]
+
+# A summary of the results
+data = {
+    "name": f"BRRT{ADHERER_VERSION} {ENVELOPE_TYPE} {ndims}d - {nsamples}",
+    "backprop-enabled": backprop_enabled,
+    "backprop-params": {"k": bp_k} if backprop_enabled else None,
+    "brrt-type": ADHERER_VERSION,
+    "brrt-params": {"d": d, "theta": delta_theta}
+    if ADHERER_VERSION == "v1"
+    else {"d": d, "delta-theta": theta0, "r": r, "N": N},
+    "meta-data": {
+        "err-count": errs,
+        "out-of-bounds-count": out_of_bounds_count,
+        "ratio": brrt.step_count / len(non_boundary_points),
+        "b-count": brrt.step_count,
+        "nonb-count": len(non_boundary_points),
+    },
+    "dimensions": [str(x) for x in range(ndims)],
+    "steps": all_exp_steps,
+    "b-nodes": [(tuple(b), tuple(n)) for b, n in brrt.boundary],
+}
+
+print("Saving results to json file...")
+with open(
+    DATA_OUTPUT,
+    "w",
+) as f:
+    f.write(json.dumps(data, indent=4))
--- examples/highway/example/highway.py
+++ examples/highway/example/highway.py
@@ -12,9 +12,13 @@ This python script serves as an example for using SUMO simulator class for the
 import os
 import sys
 from datetime import datetime
+from rtree.index import Property, Index
+from numpy import ndarray
 
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(FILE_DIR))
+
+OUTPUT_FOLDER = ".results/.saved/3d"
 
 import json
 
@@ -24,28 +28,53 @@ import pandas as pd
 import simulator
 
 import sim_bug_tools.structs as structs
+from sim_bug_tools.structs import Domain, Point
 from sim_bug_tools.exploration.boundary_core.surfacer import find_surface
-# from sim_bug_tools.exploration.brrt_std.adherer import (
-#     BoundaryAdherenceFactory, BoundaryLostException)
+
+
 # import sim_bug_tools.rng.lds.sequences as sequences
 from sim_bug_tools.exploration.brrt_std.brrt import BoundaryRRT
+
+from sim_bug_tools.exploration.brrt_std.adherer import (
+    ConstantAdherenceFactory as ConstAdherer,
+)
 from sim_bug_tools.exploration.brrt_v2.adherer import (
-    BoundaryAdherenceFactory, BoundaryLostException)
+    ExponentialAdherenceFactory as ExpAdherer,
+)
+
+from sim_bug_tools.exploration.boundary_core.adherer import (
+    BoundaryLostException,
+    SampleOutOfBoundsException,
+)
 from sim_bug_tools.graphics import Grapher
 
+ADHERER_VERSION = "v2"
 
-def to_json(name: str, num_errs: int, num_results: int, results: list[tuple[structs.Point, bool]], meta: dict = {}) -> str:
-    json_dict = {'name': name, 'num_results': num_results, 'num_errs': num_errs, 'results': [], "meta-data": meta}
-    
+
+def to_json(
+    name: str,
+    num_errs: int,
+    num_results: int,
+    results: list[tuple[structs.Point, bool]],
+    meta: dict = {},
+) -> str:
+    json_dict = {
+        "name": name,
+        "num_results": num_results,
+        "num_errs": num_errs,
+        "results": [],
+        "meta-data": meta,
+    }
+
     for p, cls in results:
-        json_dict['results'].append(
-            (
-                [x for x in p],
-                cls
-            )
-        )
-    
+        json_dict["results"].append(([x for x in p], cls))
+
     return json.dumps(json_dict, indent=4)
+
+
+def jsonable_points(points: list[structs.Point]):
+    return [[x for x in p] for p in points]
+
 
 def inject_points(path: str, grapher: Grapher):
     json_dict = None
@@ -58,149 +87,165 @@ def inject_points(path: str, grapher: Grapher):
             target.append(structs.Point(array))
         else:
             nontarget.append(structs.Point(array))
-    
+
     if len(target) > 0:
         grapher.plot_all_points(target, color="cyan")
-    
+
     if len(nontarget) > 0:
         grapher.plot_all_points(nontarget, color="orange")
-    
-    
+
+
 def get_inital_node(path: str, classifier, t0, d, domain, v):
     "Get initial node from cache or sim"
-    from os.path import exists 
+    from os.path import exists
+
     node = None
     if exists(path):
         print("Cache found, using cached initial boundary point and OSV...")
         with open(path, "r") as f:
             json_dict = json.loads(f.read())
             node = structs.Point(json_dict["b0"]), np.array(json_dict["n0"])
-            
-    else:    
+
+    else:
         print("No cached initial point, finding surface...")
         node, interm_points = find_surface(classifier, t0, d, domain, v)
         b0, n0 = node
-        json_dict = {"b0": tuple(b0.array), "n0": tuple(n0), "interm": [tuple(p.array) for p in interm_points]}
+        json_dict = {
+            "b0": tuple(b0.array),
+            "n0": tuple(n0),
+            "interm": [tuple(p.array) for p in interm_points],
+        }
         with open(path, "w") as f:
             f.write(json.dumps(json_dict, indent=4))
-    
+
     return node
-        
-    
+
 
 def main():
-    # Create a parameter manager object 
+    # Create a parameter manager object
     manager = simulator.HighwayPassTestParameterManager()
-    
+
     # Classifies a point using a test
     def classifier(p: structs.Point) -> bool:
-        # Use the manager to generate discrete concrete values 
+        # Use the manager to generate discrete concrete values
         concrete_params = manager.map_parameters(p)
         # An entire SUMO simulation will run from start-to-end
         test = simulator.HighwayPassTest(concrete_params)
         return test.scores["e_brake"] > 0 or test.scores["collision"] > 0
 
-    grapher = Grapher(True, structs.Domain.normalized(3))    
     axes_names = ["displacement", "rel. velocity", "max brake"]
-    grapher.ax.set_xlabel(axes_names[0])
-    grapher.ax.set_ylabel(axes_names[1])
-    grapher.ax.set_zlabel(axes_names[2])
-    
-    inject_points("14-25-54-results.txt", grapher)
-    
+    grapher = Grapher(True, structs.Domain.normalized(3), axes_titles=axes_names)
+
     d = 0.05
-    theta = 5 * np.pi / 180 # 5 degrees
-    delta_theta = 90 * np.pi / 180 # 5 degrees
+    theta = 15 * np.pi / 180  # 5 degrees
+    delta_theta = 90 * np.pi / 180  # 90 degrees
+    r = 2
+    N = 4
     domain = structs.Domain.normalized(3)
-    
+
     # Initial point:
     # Min distance, max rel_vel, min braking force.
-    t0 = structs.Point(0, 1, 0) 
-    
+    t0 = structs.Point(0, 1, 0)
+
     # Go in the direction of least risk to find surface
     v = np.array([1, 0, 1])
-    v = v / np.linalg.norm(v) # normalize
-    
+    v = v / np.linalg.norm(v)  # normalize
+
     # Find the surface of the envelope
-    
-    b0, n0 = get_inital_node(".3d-highway_cache-node.json", classifier, t0, d, domain, v)
+
+    b0, n0 = get_inital_node(
+        ".3d-highway_cache-node.json", classifier, t0, d, domain, v
+    )
     grapher.plot_point(b0, color="orange")
-    
-    
+
     # Create the adherence strategy
-    adherer_f = BoundaryAdherenceFactory(classifier, domain, d, delta_theta, 2, 4)
-    # adherer_f = BoundaryAdherenceFactory(classifier, structs.Domain.normalized(3), d, theta)
-    
+    if ADHERER_VERSION == "v1":
+        adherer_f = ConstAdherer(
+            classifier, Domain.normalized(3), d, theta, fail_out_of_bounds=True
+        )
+    elif ADHERER_VERSION == "v2":
+        adherer_f = ExpAdherer(
+            classifier,
+            Domain.normalized(3),
+            d,
+            delta_theta,
+            r,
+            N,
+            fail_out_of_bounds=True,
+        )
+
+    b_i = 0
+    nsamples = 200
+
+    out_of_bounds_count = 0
+    errs = 0
+    bp_k = 1
+    backprop_enabled = True
+
     # Create the BRRT
-    brrt = BoundaryRRT(b0, n0, adherer_f)    
-    done = False
-    i = 0
-    all_points = []
-    while len(input("Press enter...")) == 0:
-        points = []
-        errs = 0
-        while i < 135:
-            try:
-                p, n = brrt.expand()
-                i += 1
-                points.append(p)
-            except BoundaryLostException as e:
-                print(f"Lost boundary: {e.msg}")
-                errs += 1
-                
-                
-        grapher.plot_all_points(points, color="orange")  
-        all_points.extend(points)
-        plt.pause(0.01) 
-        
-    print("Saving results to json file")
-    meta = {
-        "total_samples": len(brrt.sub_samples),
-        "ratio": len(points) / len(brrt.sub_samples)
+    brrt = BoundaryRRT(b0, n0, adherer_f)
+    all_points: list[tuple[Point, bool]] = [(b0, True)]
+    b_i = 0
+    while brrt.step_count < nsamples:
+        try:
+            all_points.append(brrt.step())
+
+        except BoundaryLostException as e:
+            errs += 1
+
+        except SampleOutOfBoundsException as e:
+            out_of_bounds_count += 1
+
+        if b_i != brrt.step_count:
+            b_i = brrt.step_count
+            if backprop_enabled:
+                brrt.back_propegate_prev(bp_k)
+
+    # grapher.plot_all_points(list(map(lambda node: node[0], points)),
+    # color="black")
+
+    # grapher.draw_tree(brrt._tree, color="blue")
+    # grapher.save(f"{OUTPUT_FOLDER}/tree.png")
+    # plt.show()
+
+    print("Setting up output data...")
+    non_boundary_points = [
+        (p, cls)
+        for p, cls in all_points
+        if p not in map(lambda node: node[0], brrt.boundary)
+    ]
+
+    data = {
+        "name": f"BRRT{ADHERER_VERSION} 3D Scenario - {nsamples}",
+        "brrt-type": ADHERER_VERSION,
+        "brrt-params": {"d": d, "theta": theta}
+        if ADHERER_VERSION == "v1"
+        else {"d": d, "delta_theta": theta, "r": r, "N": N},
+        "backprop-enabled": backprop_enabled,
+        "backprop-params": {"k": bp_k} if backprop_enabled else None,
+        "meta-data": {
+            "errs": errs,
+            "out-of-bounds": out_of_bounds_count,
+            # Excludes samples that weren't classified / used by SUMO
+            "ratio": brrt.step_count / len(non_boundary_points),
+            "b-count": brrt.step_count,
+            "nonb-count": len(non_boundary_points),
+            "total-samples": len(all_points),
+        },
+        "dimensions": axes_names,
+        "b-nodes": [(tuple(b), tuple(n)) for b, n in brrt.boundary],
+        "nonb-points": [(tuple(p), bool(cls)) for p, cls in non_boundary_points],
     }
-    
-    with open(f"{datetime.now().strftime('%H-%M-%S')}-results.txt", "w") as f:
-        f.write(to_json("BRRT Sim Test - Boundary points only", errs, i, [(p, True) for p in all_points], meta))
+
+    print("Saving results to json file...")
+    with open(
+        f"{OUTPUT_FOLDER}/{datetime.now().strftime(f'%y%m%d-%H%M%S')}-{ADHERER_VERSION}-3d-{'' if backprop_enabled else 'no_'}bp.json",
+        "w",
+    ) as f:
+        f.write(json.dumps(data, indent=4))
+
     input("Done. Press enter to exit...")
-        
 
-    # # The parameters and scores of many tests can be grouped together into a 
-    # # dataframe
-    # print("\nRunning many tests...")
-    # points = []
-    # params = []
-    # scores = []
-    # for i in range(10):
-    #     p = rng.get_points(1)[0]
-    #     points.append(p)
-
-    #     concrete_params = manager.map_parameters(p)
-    #     params.append(concrete_params)
-
-    #     test = simulator.HighwayPassTest(concrete_params)
-    #     scores.append(test.scores)
-    #     continue
-
-    # params_df = pd.DataFrame(params)
-    # print("\n: Parameters :")
-    # print(params_df)
-
-    # scores_df = pd.DataFrame(scores)
-    # print("\n: Scores :")
-    # print(scores_df)
-
-
-
-    # # If needed, a pandas Series can be cast into a point
-    # print()
-    # p = structs.Point(params_df.iloc[0])
-    # print(p)
-
-    # # And visa versa
-    # s = p.as_series()
-    # print()
-    # print(s)
 
 if __name__ == "__main__":
-    
     main()
